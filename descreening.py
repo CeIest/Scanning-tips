437cab5d8126b9eb82a4f527df5a0d3b28b97d43
CeIest <45696416+CeIest@users.noreply.github.com>
2022-12-23 00:10:00 +0100

    Create descreening.py

---
+++ descreening.py
@@ -0,0 +1,18 @@
+import os
+import subprocess
+import pathlib 
+#https://github.com/6o6o/fft-descreen
+
+for root, _, files in os.walk('.'):
+    for file_name in files:
+        with open(os.path.join(root, file_name), 'br+') as f:
+            if file_name.endswith(".jpg"):
+                
+                filename_export = pathlib.Path(file_name).stem
+                
+                
+                commandlossless = 'python ./descreen.py "' + file_name + '" "' + filename_export + '-descreened.jpg"'
+                print("Descreening " + filename_export + "...")
+                subprocess.call(commandlossless)
+
+print("\nDescreen complete.")
