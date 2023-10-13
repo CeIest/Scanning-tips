510638f8a06f49bc4e47b846b39392afdfaf87af
CeIest <45696416+CeIest@users.noreply.github.com>
2023-10-13 11:28:21 +0200

    Update descreening.py

--- descreening.py
+++ descreening.py
@@ -1,19 +1,37 @@
-import os
+
+from tkinter.filedialog import askdirectory
+from tkinter import Tk
 import subprocess
-import pathlib 
+import pathlib
+import click
+import sys
+import os
+
 #https://github.com/6o6o/fft-descreen
 
 
-for root, _, files in os.walk('.'):
+if len(sys.argv) > 1:
+    src_folder = str(sys.argv[1]).replace("\\", "/")
+else:
+    Tk().withdraw()
+    src_folder = askdirectory()
+    print(src_folder)
+
+
+print("====================")
+
+for root, _, files in os.walk(src_folder):
     for file_name in files:
         with open(os.path.join(root, file_name), 'br+') as f:
             if file_name.endswith(".jpg"):
                 
                 filename_export = pathlib.Path(file_name).stem
                 
-                os.makedirs("Descreen", exist_ok=True)
-                commandlossless = 'descreen.py "' + file_name + '" "Descreen/' + filename_export + '.jpg"'
-                print("Descreening " + filename_export + "...")
+                os.makedirs(f"{src_folder}/Descreen", exist_ok=True)
+                commandlossless = f'descreen.py "{src_folder}/{file_name}" "{src_folder}/Descreen/{filename_export}.jpg"'
+                print(f"Descreening {filename_export}...")
                 subprocess.run(commandlossless, shell=True, check=True)
 
-print("\nDescreen complete.")
+print("====================")
+
+click.secho("\nDescreen complete.", fg="green")
